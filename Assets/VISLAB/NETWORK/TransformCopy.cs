338d6b728a61ec762bb9607ed303f6dbdd43f51f
vislab <christoffer.andersen@live.com>
2020-11-24 14:25:20 +0100

    Rename and update tranform helpers

---
+++ Assets/VISLAB/NETWORK/TransformCopy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Copies an objects position to another object, with optional rotational copy.
+/// The transform copy can also have an offset, where the offest is applied to the 
+/// object the transform is copied to. 
+/// </summary>
+public class TransformCopy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform copyFrom;
+
+    [SerializeField]
+    private Transform copyTo;
+
+    [SerializeField]
+    private Boolean rotation;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 7.5f, 0f);
+
+    private void Start()
+    {
+        if (this.copyFrom is null) throw new MissingComponentException("The object to copy from can not be null");
+        if (this.copyTo is null) throw new MissingComponentException("The object to copy to can not be null");
+    }
+
+    private void Update()
+    {
+        copyTo.position = copyFrom.position + offset;
+        if (rotation)
+        {
+            copyTo.rotation = copyFrom.rotation;
+        }
+    }
+
+}
---
+++ Assets/VISLAB/NETWORK/TransformSyncNetwork.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Synchronizes this gameobjects position and rotation from the server to all clients.
+/// Only the server will update the rotation and postion, and the clients will set the
+/// position of the gameobject to the position provided by the server.
+/// </summary>
+[NetworkSettings(sendInterval = 0.006f, channel = 2)]
+public class TransformSyncNetwork : NetworkBehaviour
+{
+    [SyncVar]
+    Quaternion newRot;
+
+    [SyncVar]
+    Vector3 newPos;
+
+    /// <summary>
+    /// Using late update to ensure all positional calculations are applied
+    /// by the engine before setting comitting to the position/rotation for this frame.
+    /// </summary>
+    void LateUpdate()
+    {
+        if (isServer)
+        {
+            this.newRot = this.transform.rotation;
+            this.newPos = this.transform.position;
+            return;
+        };
+        this.transform.position = newPos;
+        this.transform.rotation = newRot;
+
+    }
+
+}
